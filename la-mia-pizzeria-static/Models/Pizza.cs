8654b315956a0509e70ecc348b40de58ff00d0a9
giovannitamburini <giovatambu@gmail.com>
2023-09-26 16:51:54 +0200

    completed exercise without bonus

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string UrlImage { get; set; }
+        public float Price { get; set; }
+
+        public Pizza(string name, string description, string urlImage, float price)
+        {
+            this.Name = name;
+            this.Description = description;
+            this.UrlImage = urlImage;
+            this.Price = price;
+        }
+    }
+}
